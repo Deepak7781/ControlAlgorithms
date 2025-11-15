e0728a56b6fd661f3300339287572e51f485a4a7
Deepak7781 <deepak21082005@gmail.com>
2025-11-15 06:17:20 +0530

    added PID controller

--- DC_Motor/dcMotor.m
+++ DC_Motor/dcMotor.m
@@ -9,5 +9,36 @@ P_motor = K/((J*s+b)*(L*s+R)+K^2);
 
 % disp(P_motor);
 
-step(P_motor);
-ramp(P_motor);
+% step(P_motor);
+% ramp(P_motor);
+
+% kp = 100;
+% controller = pid(kp);
+% 
+% clsys = feedback(controller*P_motor, 1);
+% 
+% t = 1:0.05:5;
+% step(clsys, t);
+% grid
+% % Plot the step response of the closed-loop system
+% title('Step Response of Closed-Loop System');
+% xlabel('Time (s)');
+% ylabel('Response');
+
+
+kp = 100;
+ki = 200;
+kd = 10;
+
+controller = pid(kp, ki, kd);
+
+clsys = feedback(controller*P_motor, 1);
+t = 0:0.01:4;
+step(clsys, t);
+grid
+% Plot the step response of the closed-loop system
+title('Step Response of Closed-Loop System');
+xlabel('Time (s)');
+ylabel('Response');
+
+
