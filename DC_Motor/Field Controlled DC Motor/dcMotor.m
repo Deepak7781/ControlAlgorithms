35ef8ac17a56fe1778598c13df09c804cb0de8f9
Deepak7781 <deepak21082005@gmail.com>
2025-11-20 22:07:06 +0530

    simulink model of field controoled dc motor

--- DC_Motor/Field Controlled DC Motor/dcMotor.m
+++ DC_Motor/Field Controlled DC Motor/dcMotor.m
@@ -5,7 +5,7 @@
 
 Ktf = 0.1; % Torque constant
 Rf = 1.0;  % Field resistance
-Lf = 0.01; % Field inductance
+Lf = 0.5; % Field inductance
 J = 0.01;  % Moment of inertia
 B = 0.1;   % Damping coefficient (Viscous friction)
 
@@ -14,18 +14,20 @@ s = tf('s');
 
 plant = Ktf/((J*s + B)*(Rf + s*Lf));
 
-% disp(plant);
+disp(plant);
 
 plant_ss = ss(plant);
 
 % disp(plant_ss)
 
-step(plant);
+%step(plant);
 
-Kp = 10;
-Ki = 2;
-Kd = 5;
+Kp = 130;
+Ki = 200;
+Kd = 10;
 
 C = pid(Kp, Ki, Kd);
 
 G = feedback(C*plant_ss, 1);
+
+step(G)
