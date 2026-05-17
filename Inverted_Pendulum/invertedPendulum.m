3bb559892ae143faa3f68bcfb8c2c6c062328d14
Deepak7781 <deepak21082005@gmail.com>
2026-05-17 14:03:44 +0530

    create: invertedPendulum.m

---
+++ Inverted_Pendulum/invertedPendulum.m
@@ -0,0 +1,20 @@
+% Physical Parameters
+
+m = 0.1; % Mass of pendulum in kg
+M = 1.5; % Mass of the cart in kg
+g = 9.81; % Acceleration due to gravity
+l = 1; % Length of the pendulum in m
+
+% Building the state space model
+A = [0 1 0 0;
+     0 0 -(m*g)/M 0;
+     0 0 0 1;
+     0 0 ((M+m)*g)/(M*l) 0];
+
+B = [0; 1/M; 0; 1/(M*l)];
+
+C = eye(4);
+
+D = zeros(4,1);
+
+G = ss(A, B, C, D);
