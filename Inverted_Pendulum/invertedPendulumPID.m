707eb5ee4f6bd696d912f922d9599ae8cb947988
Deepak7781 <deepak21082005@gmail.com>
2026-05-22 22:10:36 +0530

    update: invertedPendulumPID.m

--- Inverted_Pendulum/invertedPendulumPID.m
+++ Inverted_Pendulum/invertedPendulumPID.m
@@ -6,17 +6,5 @@ g = 9.81; % Acceleration due to gravity
 l = 1; % Length of the pendulum in m
 
 % Building the state space model
-A = [0 1 0 0;
-    0 0 -(m*g)/M 0;
-    0 0 0 1;
-    0 0 ((M+m)*g)/(M*l) 0];
-
-B = [0; 1/M; 0; 1/(M*l)];
-
-C = eye(4);
-
-D = zeros(4,1);
-
-G = ss(A, B, C, D);
-
-G_tf = tf(G);
+s = tf('s');
+G = 1/((M+m)*g - M*l*s^2);
