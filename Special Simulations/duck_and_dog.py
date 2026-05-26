8b0cd35577011fcfb3f68714aa738cb73c25cd7f
Deepak7781 <deepak21082005@gmail.com>
2026-05-26 16:39:37 +0530

    minor changes

--- Special Simulations/duck_and_dog.py
+++ Special Simulations/duck_and_dog.py
@@ -62,8 +62,6 @@ def update(frame):
     dog.set_data([x_dog[frame]], [y_dog[frame]])
     start = max(0, frame - Trail_Length)
     dogTrail.set_data(x_dog[start:frame+1], y_dog[start:frame+1])
-    if R_hist[frame] <= 0.001:
-        message.set_text("Dog Caught Duck!")
     return duck, dog, dogTrail, message
 
 ani = FuncAnimation(fig, update, frames = len(Tsim), interval = dt, blit = True)
